1dce98f2518274e88a1a2cad369bfbaf585f9e3a
Virjanand <virjanand@virjanand.com>
2019-06-23 19:41:56 +0200

    name parameter string -> argument

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,8 +2,8 @@ package main.java;
 
 public class Parser {
 
-	public boolean parse(String string) {
-		if (string.equals("-l"))
+	public boolean parse(String argument) {
+		if (argument.equals("-l"))
 			return true;
 		return false;
 	}
